d2d06c0f0bbebf0242b8eb75e845a4bb0f0e91cb
Adefemi Kolawole <adefemi.kolawole@hiscox.com>
2019-07-24 14:00:37 -0400

    attempting to retain the value of the select boxes accordingly

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 function getQuote(){
-	 location.replace("http://localhost:9999/dynamic-api_client/quote");
-	 getFormValues():
+	// location.replace("http://localhost:9999/dynamic-api_client/quote");
+	 printInput();
+	 //getFormValues():
 }
 
 function getFormValues(){
@@ -10,4 +11,46 @@ function getFormValues(){
 function toggleImage(id){
 	var imghol = document.getElementById(id);
 	imghol.src = image;
+}
+function printInput() {
+  var state = document.getElementById("form").elements[0].value;
+  document.getElementById("state").innerHTML = x;
+}
+
+function updateState(state){
+	document.getElementById('state').value = state;
+}
+
+
+function displayFormContents(someForm) {
+ var out = '';
+    for (var i=0, el; el = someForm.elements[i]; i++) {
+        if (el.name) {
+            out += el.name + ' = ' + el.value + '\n';
+        }
+    }
+ alert(out);
+ printFormContents(someForm);
+}
+
+function printFormContents(someForm) {
+ var out = '';
+    for (var i=0, el; el = someForm.elements[i]; i++) {
+        if (el.name) {
+        	//el.name.style.fontweight ='900'; 
+            out += el.name.toUpperCase() + ' = ' + el.value + '\n';
+        }
+    }
+ document.getElementById('output').innerHTML = out;
+}
+
+function retainValue() {
+    var stateValue = localStorage.getItem("stateValue");
+    if(stateValue != null) {
+        $("select[name=state]").val(stateValue);
+    }
+
+    $("select[name=state]").on("change", function() {
+        localStorage.setItem("stateValue", $(this).val());
+    });
 }
