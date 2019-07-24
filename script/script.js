83fc1033d4ccb38d2c5f1b01e75c250fd538af14
Adefemi Kolawole <adefemi.kolawole@hiscox.com>
2019-07-24 11:38:23 -0400

    attempting to add image to links so the project can be understood better

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
 function getQuote(){
 	 location.replace("http://localhost:9999/dynamic-api_client/quote");
+	 getFormValues():
+}
+
+function getFormValues(){
+	let form = document.forms[0];
+	alert(form.name);
+}
+function toggleImage(id){
+	var imghol = document.getElementById(id);
+	imghol.src = image;
 }
