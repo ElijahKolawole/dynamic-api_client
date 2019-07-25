559be1461993765383656c853fc96f334c0e6f23
Adefemi Kolawole <akolawole@ggc.edu>
2019-07-25 02:17:17 -0400

    endevor to add a toggle effect to link

--- script/script.js
+++ script/script.js
@@ -54,3 +54,11 @@ function retainValue() {
         localStorage.setItem("stateValue", $(this).val());
     });
 }
+
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+          }
